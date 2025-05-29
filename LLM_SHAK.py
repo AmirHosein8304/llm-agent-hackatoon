f2a504be18b8b0f6645e98ad428de5018603c5a6
Ash <shakeriamirhossein1@gmail.com>
2025-05-29 11:35:46 +0330

    2thvhange

--- LLM_SHAK.py
+++ LLM_SHAK.py
@@ -1,13 +1,11 @@
 from openai import OpenAI
 import autogen
- 
+
 config_list = autogen.config_list_from_json(
     "OAI_CONFIG_LIST",
     filter_dict={
         "model": ["gpt-4-turbo"],
     },
 )
-metis_api_key = "tpsg-ZNJvTm228gzSX45f6HjplQVjl4P3e4H"
-client = OpenAI(api_key = metis_api_key, base_url="https://api.metisai.ir/openai/v1")
- 
- 
+
+
